d9c6fb505bb5cf1cdd66fc7237c8f75f2efd37fd
h3nr1-g <glueck.h@googlemail.com>
2015-09-29 23:55:28 +0200

    update of the communication principle sketch and update of the program structure

--- controlhost/controlhost_new/chost.py
+++ controlhost/controlhost_new/chost.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
 import argparse
+import glob
+import time
 import slaves
-
+import traceback
+import atexit
+import socket
 
 control_host = None
 board_interface_pattern = "/dev/ttyACM*"
+experiment_port = 4999
 
 
 class ControlHost(object):
@@ -14,15 +19,54 @@ class ControlHost(object):
     """
     def __init__(self, ntp_server, experiment):
         self.ntp_server = ntp_server
-        print "[+] create and start logging slaves"
+
+        print "[+] create slaves"
         self.logger = slaves.LogWriter(experiment)
-        self.logger.start()
+        self.logger.create_info_log_entry("LogWriter created successfully")
         self.npy_writer = slaves.NumpyDataWriter(experiment)
+        self.logger.create_info_log_entry("NumpyDataWriter created successfully")
+        self.broker = slaves.Broker()
+        self.logger.create_info_log_entry("Broker created successfully")
+        self.ntp_client = slaves.NTPClient(ntp_server)
+        self.logger.create_info_log_entry("NTP Client created successfully")
         self.obct = {}  # obct = OptoBoardCommunicationThread
 
+        self.experiment_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.experiment_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.status = "ready"
 
 
+    def find_boards(self,pattern):
+        """
+        method tries to find some serial interfaces with given pattern
+        :return: number of successfully created OptoBoardCommunicationThreads (equals the number of matches)
+        """
+        self.logger.create_info_log_entry("try to find some boards with the pattern: "+str(pattern))
 
+        matches=glob.glob(pattern)
+        if len(matches) < 1:
+            self.logger.create_error_log_entry("could not find any boards -> ABORT!")
+            return 0
+        self.logger.create_info_log_entry("Boards found at: "+str(matches))
+        port = slaves.opto_board_start_port
+        for interface in matches:
+            self.obct[interface] = slaves.OptoBoardCommunicationThread(port,interface)
+            port += 1
+
+        return len(self.obct)
+
+    def configure_boards(self):
+        return True
+
+    def start_experiment(self):
+        self.status = "busy"
+        while True:
+            time.sleep(1)
+
+
+    def shutdown(self):
+        self.logger.create_info_log_entry("control host is shutting down")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='control and logging application for the MR Piano')
@@ -32,6 +76,21 @@ if __name__ == "__main__":
                         default="experiment1")
     parameters = parser.parse_args()
 
-
     print "[+] start control host for MR Piano"
-    control_host = ControlHost(parameters.server_ip, parameters.name)
+    global control_host, board_interface_pattern
+    try:
+        # try to create the control host
+        control_host = ControlHost(parameters.server_ip, parameters.name)
+        atexit.register(control_host.shutdown)
+
+    except Exception as e:
+        print(traceback.format_exc())
+        control_host.logger.create_error_log_entry("failed to initialize control host -> ABORT!")
+        exit(-1)
+
+    # try to find some boards
+    if control_host.find_boards(board_interface_pattern) < 1 and not control_host.configure_boards():
+        exit(-1)
+
+    control_host.start_experiment()
+
--- controlhost/controlhost_new/slaves.py
+++ controlhost/controlhost_new/slaves.py
@@ -1,5 +1,25 @@
+import socket
 import threading
 import logging
+import zmq
+import os
+import datetime
+
+#some constants and value definitions
+log_subdirectory = "data"
+broker_subscriber_port = 5000
+broker_publisher_port = 5001
+log_writer_port = 5002
+numpy_writer_port = 5003
+opto_board_start_port = 5004
+
+# 'global' variables for external access
+logger = None
+broker = None
+experiment_handler = None
+numpy_writer = None
+opto_board_communication_thread = {}
+
 
 class Broker(threading.Thread):
     """
@@ -7,31 +27,83 @@ class Broker(threading.Thread):
     subscribed WriterThreads
     """
     def __init__(self):
-         threading.Thread.__init__(self)
+        threading.Thread.__init__(self)
+        context = zmq.Context()
+        self.subscriber_socket = context.socket(zmq.SUB)
+        self.publisher_socket = context.socket(zmq.PUB)
 
     def run(self):
         pass
 
-
-class ExperimentHandler(threading.Thread):
-    """
-    class for the information exchange with sender / experiment script
-    """
-    def __init__(self):
-         threading.Thread.__init__(self)
-
-    def run(self):
-        pass
 
 class LogWriter(threading.Thread):
     """
     This class is responsible for the creation of the *.log files
     It uses ZMQ based subscribe socket writes the incoming data in a log file
     """
+
+    class Formatter(logging.Formatter):
+        """
+        sub class of logging.Formater for the individual log format
+        """
+        converter=datetime.datetime.fromtimestamp
+        def formatTime(self, record, datefmt=None):
+            ct = self.converter(record.created)
+            if datefmt:
+                s = ct.strftime(datefmt)
+            else:
+                t = ct.strftime("%Y-%m-%d %H:%M:%S")
+                s = "%s,%03d" % (t, record.msecs)
+            return s
+
+
     def __init__(self, filename):
         threading.Thread.__init__(self)
-        self.logger = logging.getLogger('PianoLogger')
+
+        #create log directory and file if neccessary
+        target_dir = os.getcwd()+"/"+log_subdirectory
+        if not os.path.exists(target_dir):
+            os.mkdir(target_dir)
+
+        logfile = target_dir+"/"+filename+".log"
+
+        self.logger = logging.getLogger('LogWriter')
         self.logger.setLevel(logging.DEBUG)
+        self.fh=logging.FileHandler(logfile)
+        self.fh.setLevel(logging.DEBUG)
+        self.sh=logging.StreamHandler()
+        self.sh.setLevel(logging.DEBUG)
+        self.formatter = LogWriter.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%Y-%m-%d,%H:%M:%S.%f')
+        self.formatter.datefmt='%Y-%m-%d,%H:%M:%S--%s.%f'
+        self.fh.setFormatter(self.formatter)
+        self.sh.setFormatter(self.formatter)
+        self.logger.addHandler(self.fh)
+        self.logger.addHandler(self.sh)
+        self.logger.propagate = False
+
+        #create the zmq socket, we bind it later
+        context = zmq.Context()
+        self.socket = context.socket(zmq.SUB)
+
+        self.lock = threading.RLock()
+        global logger
+        logger = self
+
+
+    def create_info_log_entry(self,msg,sender="LogWriter"):
+        """
+        method provides the possibility to create an info log entry
+        """
+        with self.lock:
+            self.logger.info(msg)
+
+    def create_error_log_entry(self,msg,sender="LogWriter"):
+        """
+        method provides the possibility to create an error log entry
+        """
+        with self.lock:
+            self.logger.error(msg)
+
         
     def run(self):
         pass
@@ -44,10 +116,26 @@ class NumpyDataWriter(threading.Thread):
     """
     def __init__(self, filename):
         threading.Thread.__init__(self)
-        self.filename = filename
+        self.basefilename = filename
+        self.block_counter = 0
+
+        #create target directory if neccessary
+        target_dir = os.getcwd()+"/"+log_subdirectory
+        if not os.path.exists(target_dir):
+            os.mkdir(target_dir)
+
+        #create the zmq socket, we bind it later
+        context = zmq.Context()
+        self.socket = context.socket(zmq.SUB)
+
+        self.lock = threading.RLock()
+
+        global numpy_writer
+        numpy_writer = self
 
     def run(self):
         pass
+
 
 class OptoBoardCommunicationThread(threading.Thread):
     """
@@ -59,5 +147,16 @@ class OptoBoardCommunicationThread(threading.Thread):
         self.port = int(port)
         self.device = device
 
+        global opto_board_communication_thread
+        opto_board_communication_thread[device] = self
+
     def run(self):
         pass
+
+
+class NTPClient(object):
+    """
+    simple ntp client for the time synchronisation of the boards
+    """
+    def __init__(self,ip):
+        self.server_ip = ip
